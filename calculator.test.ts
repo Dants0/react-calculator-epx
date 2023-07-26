780d3fb5cafc8d933a756e86d7c36b573afc148b
Dants0 <gui.dg1@hotmail.com>
2023-07-26 11:20:35 -0300

    (update and feature): update to version #1.0.3 and fibonacci sequence and factorial was be added in module

--- calculator.test.ts
+++ calculator.test.ts
@@ -32,3 +32,81 @@ describe('Calculator', () => {
     expect(()=>calculator.division()).toThrowError('Division by zero is not possible');
   });
 });
+
+describe('Calculator - showResults()', () => {
+
+  test('showResults() should throw an error when no results are available',()=>{
+    const calculator = new Calculator({num_1:0, num_2:0})
+    expect(()=>calculator.showResults()).toThrowError('It is not possible to present results without calculating first')
+  })
+  
+  test('showResults() should return formatted correctly', ()=>{
+    const calculator = new Calculator({num_1:20, num_2:10})
+
+    const resultsExpected = `Sum: 30\nMinus: 10\nMultiply: 200\nDivision: 2`
+
+    expect(calculator.showResults()).toBe(resultsExpected)
+  })
+
+})
+
+
+describe('Calculator Plus Features', () => {
+  
+  test('fibonacci() should return the fibonacci correctly sequence for n = 2', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(calculator.fibonacci(2)).toBe(1)
+  })
+
+  test('fibonacci() should return the fibonacci correctly sequence for n = 10', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(calculator.fibonacci(10)).toBe(55)
+  })
+
+  test('fibonacci() should return the fibonacci correctly sequence for n = 8', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(calculator.fibonacci(8)).toBe(21)
+  })
+
+  test('fibonacci() should return the fibonacci correctly sequence for n = 1', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(calculator.fibonacci(1)).toBe(1)
+  })
+
+  test('fibonacci() should return the fibonacci correctly sequence for n = 20', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(calculator.fibonacci(20)).toBe(6765)
+  })
+  
+  test('fibonacci() throws an error if the fibonacci sequence is invalid when n = -1', () => {
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(()=>calculator.fibonacci(-1)).toThrowError('Enter a positive integer')
+  })
+
+  test('fibonacci() throws an error if the fibonacci sequence is invalid when n = 0', ()=>{
+    const calculator = new Calculator({ num_1: 0, num_2:0})
+    expect(()=>calculator.fibonacci(0)).toThrowError('Enter a positive integer')
+  })
+
+  test('factorial() should return the correctly factorial value when n = 4', () =>{
+    const calculator = new Calculator({ num_1: 0, num_2: 0})
+    expect(calculator.factorial(4)).toBe(24)
+  })
+
+  test('factorial() should return 1 when n = 0', () =>{
+    const calculator = new Calculator({ num_1: 0, num_2: 0})
+    expect(calculator.factorial(0)).toBe(1)
+  })
+
+  test('factorial() should return 1 when n = 1', () =>{
+    const calculator = new Calculator({ num_1: 0, num_2: 0})
+    expect(calculator.factorial(1)).toBe(1)
+  })
+
+  test('factorial() throws an error when n is negative number', () =>{
+    const calculator = new Calculator({ num_1: 0, num_2: 0})
+    expect(()=>calculator.factorial(-1)).toThrowError('Factorial is not possible for negative numbers')
+  })
+
+
+})
